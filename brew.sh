77bac653c1b3a7c48f1e7f6b09b7644c58790716
Bret Walker <bretwalker@users.noreply.github.com>
2020-01-10 22:43:59 -0500

    Install VS Code

--- brew.sh
+++ brew.sh
@@ -94,6 +94,8 @@ brew cask install docker
 
 brew cask install Slack
 
+brew cask install visual-studio-code
+
 brew cask install iterm2
 echo 'Now go set iTerm font to a Powerline font and set color preset to Solarized Dark'
 
